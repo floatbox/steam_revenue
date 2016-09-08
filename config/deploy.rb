2d5b23df8bdec48d4eaf7c03365b3cc1fd67bffa
Andrey Arefiev <arefiev@gmail.com>
2016-09-08 21:52:51 +0300

    Config rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,3 +34,14 @@ set :deploy_to, '~/apps/steam_revenue'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1p112'
+
+# in case you want to set ruby version from the file:
+# set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
